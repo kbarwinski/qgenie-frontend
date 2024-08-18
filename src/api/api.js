094a72d0f59598509bba50cfdba7c53f61b9635e
kbarwinski <kajetanbarwinski@gmail.com>
2024-08-18 04:45:02 +0200

    api base url fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://your-api-base-url";
+const API_BASE_URL = "http://localhost:5020";
 
 export const getOrCreateInterview = async (interviewId) => {
   try {
